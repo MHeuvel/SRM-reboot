45911fa574811d874945377ff23659721b1158c8
MHeuvel <31540749+MHeuvel@users.noreply.github.com>
2026-03-09 10:42:56 +0100

    Create const.py

---
+++ custom_components/srm_reboot/const.py
@@ -0,0 +1,14 @@
+DOMAIN = "srm_reboot"
+CONF_IP = "ip"
+CONF_PORT = "port"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+DEFAULT_PORT = 8001
+SERVICE_REBOOT = "reboot"
+ATTR_ENTRY_ID = "entry_id"
+INGRESS_MISSED = "ingress_missed"
+INGRESS_MISSED_LIMIT = "ingress_missed_limit"
+INGRESS_BLOCKED = "ingress_blocked"
+
+SERVICE_INCREASE_INGRESS_MISSED = "increase_ingress_missed"
+SERVICE_RESET_INGRESS_MISSED = "reset_ingress_missed"
